f45d2fc1e6650291f53ce313f76e81a1b401e584
Hamid Bekamiri <HamidB@business.aau.dk>
2022-01-04 06:46:56 +0100

    Update app_new.py

--- app_new.py
+++ app_new.py
@@ -280,7 +280,7 @@ def df_clean (df_inter):
     return df_all, freq, less_freq
 
 st.write("A Sample of the CSV")
-df_sample = pd.read_csv('NLP_CA_Dataset.csv')
+df_sample = pd.read_csv('df_nlp_ca.csv')
 st.dataframe(df_sample.head(2))
 
 uploaded_file_inter = st.file_uploader("Upload CSV file of Interviewees!")
@@ -388,40 +388,40 @@ if uploaded_file_inter is not None:
 
 
 
-st.markdown('<h1 style="background-color: gainsboro; padding-left: 10px; padding-bottom: 20px;">Indicator Search Engine</h1>', unsafe_allow_html=True)
-df_example = st.text_input('', help='Enter the search string and hit Enter/Return')
+# st.markdown('<h1 style="background-color: gainsboro; padding-left: 10px; padding-bottom: 20px;">Indicator Search Engine</h1>', unsafe_allow_html=True)
+# df_example = st.text_input('', help='Enter the search string and hit Enter/Return')
 
-# uploaded_file_example = st.file_uploader("Upload CSV file of Indicator example!")
-# if uploaded_file_example is not None:    
-#     #read csv
-#     df_example = pd.read_csv(uploaded_file_example)
-#     df_example.head()
+# # uploaded_file_example = st.file_uploader("Upload CSV file of Indicator example!")
+# # if uploaded_file_example is not None:    
+# #     #read csv
+# #     df_example = pd.read_csv(uploaded_file_example)
+# #     df_example.head()
 
-if st.button("Search"):
+# if st.button("Search"):
 
-    model = SentenceTransformer('all-MiniLM-L6-v2')
+#     model = SentenceTransformer('all-MiniLM-L6-v2')
 
-    sentences_example = df_example
-    sentences_docs = df_docs.paragraph_text.to_list()
+#     sentences_example = df_example
+#     sentences_docs = df_docs.paragraph_text.to_list()
 
-    embeddings_example = model.encode(sentences_example)
-    embeddings_docs = model.encode(sentences_docs)
+#     embeddings_example = model.encode(sentences_example)
+#     embeddings_docs = model.encode(sentences_docs)
 
-    df_results = get_top_n_similar_patents_df(embeddings_example, embeddings_docs)
-    st.table(df_results)
-    # st.download_button("Press to Download", df_results,"file.csv","text/csv",key='download-csv')
-    st.download_button(label='📥 Download Current Result', data=convert_df_to_csv(df_results), file_name= 'df_results.csv')
+#     df_results = get_top_n_similar_patents_df(embeddings_example, embeddings_docs)
+#     st.table(df_results)
+#     # st.download_button("Press to Download", df_results,"file.csv","text/csv",key='download-csv')
+#     st.download_button(label='📥 Download Current Result', data=convert_df_to_csv(df_results), file_name= 'df_results.csv')
     
-    #Store sentences & embeddings on disc
-    with open('embeddings_docs.pkl', "wb") as fOut:
-        pickle.dump({'sentences': sentences_docs, 'embeddings': embeddings_docs}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
+#     #Store sentences & embeddings on disc
+#     with open('embeddings_docs.pkl', "wb") as fOut:
+#         pickle.dump({'sentences': sentences_docs, 'embeddings': embeddings_docs}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
 
-    #Store sentences & embeddings on disc
-    with open('embeddings_example.pkl', "wb") as fOut:
-        pickle.dump({'sentences': sentences_example, 'embeddings': embeddings_example}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
+#     #Store sentences & embeddings on disc
+#     with open('embeddings_example.pkl', "wb") as fOut:
+#         pickle.dump({'sentences': sentences_example, 'embeddings': embeddings_example}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
 
-    #Load sentences & embeddings from disc
-    with open('embeddings_example.pkl', "rb") as fIn:
-        stored_data = pickle.load(fIn)
-        stored_sentences = stored_data['sentences']
-        stored_embeddings = stored_data['embeddings']
+#     #Load sentences & embeddings from disc
+#     with open('embeddings_example.pkl', "rb") as fIn:
+#         stored_data = pickle.load(fIn)
+#         stored_sentences = stored_data['sentences']
+#         stored_embeddings = stored_data['embeddings']
